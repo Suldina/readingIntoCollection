dd8afd4f2b5a658ad174633d5c8d657e761836c2
Suldina <elenasuldina@mail.ru>
2017-08-14 22:19:51 +0300

    second commit-draft

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,28 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-
-    Scanner.read("c:/123.txt")
-        List<String> list = new LinkedList<>(Arrays.asList(s.split(" ")));
+        String str;
+    }
+     Scanner str = new Scanner.read("c:/123.txt")
+        List<String> list = new LinkedList<>(Arrays.asList(str.split(" ")));
         int i = 0;
         for(int n = 0; n < list.size(); n=0)	{
             String st = list.get(n);
             Iterator<String> iter = list.iterator();
             while(iter.hasNext())	{
-                String str = iter.next();
-                if(st.equals(str))	{
+                String st = iter.next();
+                if(str.equals(str))	{
                     i++;
 
                     iter.remove();
                 }
             }
-            System.out.println(st + " : " + i);
+            System.out.println(str + " : " + i);
             //System.out.println(list);
+
             i = 0;
     }
 }
